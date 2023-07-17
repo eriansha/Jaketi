197ceb9eb2b25c4552d286afd09d3c7173efe9ee
Ivan Putra Eriansya <eriansha.van@gmail.com>
2023-07-17 18:36:18 +0700

    Initial Commit

---
+++ Jaketi/JaketiApp.swift
@@ -0,0 +1,17 @@
+//
+//  JaketiApp.swift
+//  Jaketi
+//
+//  Created by Ivan on 17/07/23.
+//
+
+import SwiftUI
+
+@main
+struct JaketiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
